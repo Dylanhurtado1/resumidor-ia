952fa1968486565d23b72e2d40e15161335c57d7
Dylan <dhurtado@frba.utn.edu.ar>
2025-06-29 18:44:02 -0300

    Primer commit backend: estructura base, gitignore y primer código (#1)
    Co-authored-by: dyhurtado_meli <dylan.hurtado@mercadolibre.com>

---
+++ Backend/src/main/java/com/example/resumidor_ia/ResumidorIaApplication.java
@@ -0,0 +1,13 @@
+package com.example.resumidor_ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumidorIaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumidorIaApplication.class, args);
+	}
+
+}
